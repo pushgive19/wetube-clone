83a5648f4c3ce4f99e485a66429b4231abca46eb
pushgive19 <59918189+pushgive19@users.noreply.github.com>
2020-10-30 13:03:06 +0900

    maek server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");CSSMediaRule
+const express = require('express')
+// require은 express를 node_modules에서 가져오는 거야
+const app = express()
+// app 초기화? 실행?
+const PORT = 4000;
+
+function handleListening (){
+  console.log(`Listening on:http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
